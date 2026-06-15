6904c338ef219bd1bca2ec9c6dbe92679605c462
nile <niletechno16@gmail.com>
2026-06-16 02:35:01 +0300

    65465

--- django_app/apps/dashboard/views.py
+++ django_app/apps/dashboard/views.py
@@ -244,7 +244,7 @@ def home(request):
         common_problems = [
             {
                 'classification': r.get('category_name', ''),
-                'category_id':    r.get('id', ''),
+                'category_id':    r.get('category_id', ''),
                 'total':          r.get('TotalIssues',   0) or 0,
             }
             for r in raw_issues
